38b34a91054c1d6d81bb431849c002a626e61327
Antonio <asoriano@appdev.yearup.org>
2024-06-20 11:24:46 -0500

    complete exercise 3

--- es6_classes.js
+++ es6_classes.js
@@ -21,15 +21,26 @@ class Employee {
         this.jobTitle = newJobTitle;
         this.payRate = newPayRate;
     }
+
+    // method from workbook
+    getIntro() {
+        let intro =
+            "Hi! I'm " + this.getFullName() + " and I am a " +
+            this.jobTitle;
+        return intro;
+    }
 }
 
 // create employee 1 from the Employee class (blueprint) using the "new" keyword
 let employee1 = new Employee(1, "Ian", "Auston", "Graphic Artist", 42.50);
 let employee2 = new Employee(2, "Mike", "Schmidt", "Secruity Guard", 4);
 
+// introduce employee 1
+let intro1 = employee1.getIntro();
+console.log(intro1);
+
 // new promote method with new job title and pay rate
 employee1.promote("Sr. Graphic Artist", 46.75);
-employee2.promote("Head Security Officer", 4.01);
 
 // print out some info about employee 1
 console.log(`Employee ${employee1.getFullName()} created`);
@@ -38,6 +49,13 @@ console.log(`Pay rate is $${employee1.payRate}`);
 
 // break for readability in console
 console.log("---------------------------------------")
+
+// introduce employee 2
+let intro2 = employee2.getIntro();
+console.log(intro2);
+
+// new promote method with new job title and pay rate
+employee2.promote("Head Security Officer", 4.01);
 
 // print out some info about employee 2
 console.log(`Employee ${employee2.getFullName()} created`);
